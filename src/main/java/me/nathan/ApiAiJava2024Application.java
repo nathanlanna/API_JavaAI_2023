3ae513545727921bf229d97dfd809dfd9ff389f6
nathanlanna <nathan.lanna0801@gmail.com>
2024-07-10 15:13:47 -0300

    primeiro commit criação

---
+++ src/main/java/me/nathan/ApiAiJava2024Application.java
@@ -0,0 +1,13 @@
+package me.nathan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAiJava2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAiJava2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/me/nathan/ApiAiJava2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.nathan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAiJava2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
